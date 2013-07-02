28ca8931d6f0fcec52d4f63ce0fc32d3d2c91279
Matthew Turk <matthewturk@gmail.com>
2013-07-02 17:34:04 -0400

    Adding Gasoline and PKDGrav to script-all.py.

--- script-all.py
+++ script-all.py
@@ -48,6 +48,8 @@ fo = filter_registry["finest"][0]
 do_enzo = True
 do_ramses = True
 do_gadget = True
+do_gasoline = True
+do_pkdgrav = True
 
 if do_enzo:
     ds_enzo = load("DD0040/data0040")
@@ -75,3 +77,30 @@ if do_gadget:
     ds_gadget.h._derived_fields_add(df)
     center = np.array([ 29.75540543, 32.12417221, 28.28912735])  # Gadget unit system: [0, 60]
     process_dataset(ds_gadget, center)
+
+if do_gasoline:
+    cosmology_parameters = dict(current_redshift = 0.0, omega_lambda = 0.728,
+        omega_matter = 0.272, hubble_constant = 0.702)
+    ds_gasoline = TipsyStaticOutput("agora_1e11.00400",
+        cosmology_parameters = cosmology_parameters,
+        unit_base = {'mpchcm': 1.0/60.0})
+    ds_gasoline.add_particle_filter(fo)
+    df = particle_deposition_functions("finest",
+        "Coordinates", "Mass", TipsyFieldInfo)
+    ds_gasoline.h._derived_fields_add(df)
+    center = np.array([-0.014738, 0.026979, -0.010535])
+    process_dataset(ds_gasoline, center)
+
+if do_pkdgrav:
+    cosmology_parameters = dict(current_redshift = 0.0, omega_lambda = 0.728,
+        omega_matter = 0.272, hubble_constant = 0.702)
+    ds_pkdgrav = TipsyStaticOutput("halo1e11_run1.00400", endian="<",
+        field_dtypes = {"Coordinates": "d"},
+        cosmology_parameters = cosmology_parameters,
+        unit_base = {'mpchcm': 1.0/60.0})
+    ds_pkdgrav.add_particle_filter(fo)
+    df = particle_deposition_functions("finest",
+        "Coordinates", "Mass", TipsyFieldInfo)
+    ds_pkdgrav.h._derived_fields_add(df)
+    center = np.array([0.00914883, 0.0242125, -0.04274633])
+    process_dataset(ds_pkdgrav, center)
