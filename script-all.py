098848d7299d6b66e0c3a7e77fba39d1ddd7ef7d
Matthew Turk <matthewturk@gmail.com>
2013-07-15 18:43:15 -0400

    Making Agora analysis script easier to run.

--- script-all.py
+++ script-all.py
@@ -28,89 +28,81 @@ from yt.frontends.stream.data_structures import \
 from yt.data_objects.particle_fields import \
     particle_deposition_functions, \
     particle_vector_functions
+from yt.startup_tasks import YTParser, unparsed_args
 
 @particle_filter("finest", ["ParticleMassMsun"])
 def finest(pfilter, data):
     return data["ParticleMassMsun"] < 340000
 
-def main():
-    # CHOOSE DATASETS YOU WANT TO PROCESS
-    do_art2 = False
-    do_enzo = False
-    do_ramses = False
-    do_gadget = False
-    do_gasoline = False
-    do_pkdgrav = False
-    do_particles = False
+output_functions = {}
+def register_output_function(func):
+    output_functions[func.func_name[3:]] = func
 
-    if do_art2:
-        ds_art2 = load("A11QR1/s11Qzm1h2_a1.0000.art")
-        ds_art2.add_particle_filter("finest")
-        center = 128 * np.array([0.492470,  0.533444,  0.476942]) 
-        process_dataset(ds_art2, center)
+@register_output_function
+def do_art2():
+    ds_art2 = load("A11QR1/s11Qzm1h2_a1.0000.art")
+    center = 128 * np.array([0.492470,  0.533444,  0.476942]) 
+    process_dataset(ds_art2, center)
+    return ds_art2
 
-    if do_enzo:
-        ds_enzo = load("./DD0040/data0040")
-        ds_enzo.add_particle_filter("finest")
-        center = np.array([ 0.49297869, 0.50791068, 0.50727271])
-        process_dataset(ds_enzo, center)
-        
-    if do_ramses:
-        ds_ramses = load("./output_00101/info_00101.txt")
-        ds_ramses.add_particle_filter("finest")
-        center = np.array([ 0.48598457, 0.52665735, 0.48984628])
-        process_dataset(ds_ramses, center)
+@register_output_function
+def do_enzo():
+    ds_enzo = load("./DD0040/data0040")
+    center = np.array([ 0.49297869, 0.50791068, 0.50727271])
+    process_dataset(ds_enzo, center)
 
-    if do_gadget:
-        ds_gadget = GadgetStaticOutput("./snapshot_010", unit_base = {"mpchcm": 1.0})
-        ds_gadget.add_particle_filter("finest")
-        center = np.array([ 29.75540543, 32.12417221, 28.28912735])
-        process_dataset(ds_gadget, center)
 
-    if do_gasoline:
-        cosmology_parameters = dict(current_redshift = 0.0, omega_lambda = 0.728,
-                                    omega_matter = 0.272, hubble_constant = 0.702)
-        ds_gasoline = TipsyStaticOutput("./agora_1e11.00400",
-                                        cosmology_parameters = cosmology_parameters,
-                                        unit_base = {'mpchcm': 1.0/60.0})
-        ds_gasoline.add_particle_filter("finest")
-        center = np.array([-0.014738, 0.026979, -0.010535])
-        #center = np.array([-0.01477163, 0.02694199, -0.0105199])
-        process_dataset(ds_gasoline, center)
+@register_output_function
+def do_ramses():
+    ds_ramses = load("./output_00101/info_00101.txt")
+    center = np.array([ 0.48598457, 0.52665735, 0.48984628])
+    process_dataset(ds_ramses, center)
 
-    if do_pkdgrav:
-        cosmology_parameters = dict(current_redshift = 0.0, omega_lambda = 0.728,
-                                    omega_matter = 0.272, hubble_constant = 0.702)
-        ds_pkdgrav = TipsyStaticOutput("./halo1e11_run1.00400", endian="<",
-                                       field_dtypes = {"Coordinates": "d"},
-                                       cosmology_parameters = cosmology_parameters,
-                                       unit_base = {'mpchcm': 1.0/60.0})
-        ds_pkdgrav.add_particle_filter("finest")
-        center = np.array([-0.01434195, 0.027505, -0.01086525])
-        process_dataset(ds_pkdgrav, center)
+@register_output_function
+def do_gadget():
+    ds_gadget = GadgetStaticOutput("./snapshot_010", unit_base = {"mpchcm": 1.0})
+    center = np.array([ 29.75540543, 32.12417221, 28.28912735])
+    process_dataset(ds_gadget, center)
 
-    if do_particles:
-        f = h5py.File("./s11Qzm1h2_a1.0000.art.h5")
-        data = dict((k, f[k][:].astype("float64")) for k in f)
-        bbox = np.array([[0.0, 128.0], [0.0, 128.0], [0.0, 128.0]])
-        data["particle_mass"] *= 2.2023338912587828e+43 # Convert to grams
-        ds_particles = load_particles(data, 2.0604661199638546e+24, bbox=bbox)
-        ds_particles.add_particle_filter("finest")
-        particle_vector_functions("all", ["particle_position_%s" % ax for ax in 'xyz'],
-                                  ["particle_velocity_%s" % ax for ax in 'xyz'], StreamFieldInfo)
-        particle_vector_functions("finest", ["particle_position_%s" % ax for ax in 'xyz'],
-                                  ["particle_velocity_%s" % ax for ax in 'xyz'], StreamFieldInfo)
-        particle_deposition_functions("all", "Coordinates", "particle_mass", StreamFieldInfo)
-        particle_deposition_functions("finest", "Coordinates", "particle_mass", StreamFieldInfo)
-        center = np.array([0.492470,  0.533444,  0.476942]) 
-        center *= 128
-        process_dataset(ds_particles, center)
+@register_output_function
+def do_gasoline():
+    cosmology_parameters = dict(current_redshift = 0.0, omega_lambda = 0.728,
+                                omega_matter = 0.272, hubble_constant = 0.702)
+    ds_gasoline = TipsyStaticOutput("./agora_1e11.00400",
+                                    cosmology_parameters = cosmology_parameters,
+                                    unit_base = {'mpchcm': 1.0/60.0})
+    center = np.array([-0.014738, 0.026979, -0.010535])
+    #center = np.array([-0.01477163, 0.02694199, -0.0105199])
+    process_dataset(ds_gasoline, center)
+
+@register_output_function
+def do_pkdgrav():
+    cosmology_parameters = dict(current_redshift = 0.0, omega_lambda = 0.728,
+                                omega_matter = 0.272, hubble_constant = 0.702)
+    ds_pkdgrav = TipsyStaticOutput("./halo1e11_run1.00400", endian="<",
+                                   field_dtypes = {"Coordinates": "d"},
+                                   cosmology_parameters = cosmology_parameters,
+                                   unit_base = {'mpchcm': 1.0/60.0})
+    center = np.array([-0.01434195, 0.027505, -0.01086525])
+    process_dataset(ds_pkdgrav, center)
+
+@register_output_function
+def do_particles():
+    f = h5py.File("./s11Qzm1h2_a1.0000.art.h5")
+    data = dict((k, f[k][:].astype("float64")) for k in f)
+    bbox = np.array([[0.0, 128.0], [0.0, 128.0], [0.0, 128.0]])
+    data["particle_mass"] *= 2.2023338912587828e+43 # Convert to grams
+    ds_particles = load_particles(data, 2.0604661199638546e+24, bbox=bbox)
+    center = np.array([0.492470,  0.533444,  0.476942]) 
+    center *= 128
+    process_dataset(ds_particles, center)
 
 def process_dataset(ds, center):
 
     #=======================
     #  [1] TOTAL MASS
     #=======================
+    ds.add_particle_filter("finest")
     sp = ds.h.sphere(center, (1.0, 'mpc'))
     total_particle_mass = sp.quantities["TotalQuantity"]( ("all","ParticleMassMsun") )[0]
     print "Total particle mass within a radius of 1 Mpc of the center: %0.3e Msun" \
@@ -212,11 +204,29 @@ def process_dataset(ds, center):
         halos.write_out("./images/%s_MergerHalos.out" % ds)
         
         source = ds.h.region(center, center - (w[0]/ds[w[1]])/2.0, center + (w[0]/ds[w[1]])/2.0)
-        proj = ds.h.proj(field, 2, weight_field = field, data_source = source)
+        proj = ds.h.proj(field, "z", weight_field = field, data_source = source)
         pw = proj.to_pw(fields = [field], center = center, width = w)
         pw.set_zlim(field, 1e-31, 1e-24)
         pw.annotate_hop_circles(halos, annotate=True, print_halo_mass=False, min_size=1000)
         pw.save("./images/%s_Projection_z_%s_subset.png" % (ds, field[1]))
 
 if __name__ == '__main__':
-    main()
+    parser = YTParser(description = 'AGORA analysis')
+    parser.add_argument("--run-all", action="store_true", dest = "run_all")
+    for output_type in sorted(output_functions):
+        parser.add_argument("--run-%s" % output_type,
+                dest = "outputs",
+                action = "append_const",
+                const = output_type,
+                )
+    opts = parser.parse_args(unparsed_args)
+    if opts.run_all:
+        outputs = output_functions.keys()
+    else:
+        outputs = opts.outputs
+    if outputs is None:
+        parser.error("No outputs supplied.")
+        sys.exit()
+    for output in sorted(outputs):  
+        mylog.info("Examining %s", output)
+        output_functions[output]()
