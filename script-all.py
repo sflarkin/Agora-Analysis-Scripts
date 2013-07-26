51ed6a2f828184e0114f8c106354a37f5ed74099
Ji-hoon Kim <mornkr@slac.stanford.edu>
2013-07-25 21:08:04 -0700

    minor fixes

--- script-all.py
+++ script-all.py
@@ -40,10 +40,10 @@ def register_output_function(func):
 
 @register_output_function
 def do_art2():
-    ds_art2 = ARTIOStaticOutput("../RUN.4R4/agora.nb_a1.0018.art")
-    center = np.array([63.05873108, 68.22652435, 61.08340073])
-    #ds_art2 = load("./A11QR1/s11Qzm1h2_a1.0000.art")
-    #center = 128 * np.array([0.492470,  0.533444,  0.476942]) 
+    ds_art2 = load("./A11QR1/s11Qzm1h2_a1.0000.art")
+    center = 128 * np.array([0.492470,  0.533444,  0.476942]) 
+    #ds_art2 = ARTIOStaticOutput("../RUN.4R4/agora.nb_a1.0018.art")
+    #center = np.array([63.05873108, 68.22652435, 61.08340073])
     ds_art2.add_particle_filter("finest")
     process_dataset(ds_art2, center)
     return ds_art2
@@ -96,13 +96,14 @@ def do_pkdgrav():
 
 @register_output_function
 def do_particles():
-    f = h5py.File("../agora.nb_a1.0018.art.h5")
-    #f = h5py.File("../s11Qzm1h2_a1.0000.art.h5")
+    f = h5py.File("../s11Qzm1h2_a1.0000.art.h5")
+    #f = h5py.File("../agora.nb_a1.0018.art.h5")
     data = dict((k, f[k][:].astype("float64")) for k in f)
     bbox = np.array([[0.0, 128.0], [0.0, 128.0], [0.0, 128.0]])
-    data["particle_mass"] *= 2.2023338912587828e+43 # Convert to grams
+    data["particle_mass"] *= 2.2023338912587828e+43 # Convert to grams, only needed for s11Qzm1h2_a1.0000.art.h5
     ds_particles = load_particles(data, 2.0604661199638546e+24, bbox=bbox)
     center = 128 * np.array([0.492470,  0.533444,  0.476942]) 
+    #center = np.array([63.05873108, 68.22652435, 61.08340073])
     ds_particles.add_particle_filter("finest")
     particle_vector_functions("all",
                               ["particle_position_%s" % ax for ax in 'xyz'],
